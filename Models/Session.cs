fd97aafac14e78aee5c7b0c0a10ad00b8b84e9f4
ShalasH <User@SHALASH>
2026-06-11 03:42:20 +0300

    комментарии

--- Models/Session.cs
+++ Models/Session.cs
@@ -9,12 +9,12 @@ namespace Kino.Models
     // Модель сеанса
     public class Session
     {
-        public int Id { get; set; }
-        public int FilmId { get; set; } // Связь с фильмом
-        public string FilmName { get; set; } // Для отображения
-        public DateTime Date { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public string Hall { get; set; }
-        public int FreeSeats { get; set; }
+        public int Id { get; set; } // Используется для поиска и удаления конкретного сеанса
+        public int FilmId { get; set; } // Связывает сеанс с конкретным фильмом
+        public string FilmName { get; set; } // Для отображения названия фильма 
+        public DateTime Date { get; set; } // Дата проведения сеанса
+        public TimeSpan StartTime { get; set; } //Время начала сеанса
+        public string Hall { get; set; } // Название кинозала
+        public int FreeSeats { get; set; } //Количество свободных мест в зале на этот сеанс
     }
 }
